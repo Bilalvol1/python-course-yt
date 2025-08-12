168b92da071cdced024d22a262259736cd7b2721
Bilal Shaikh <vol1shaikh@gmail.com>
2025-08-12 15:29:13 +0530

    Commit

--- program7.py
+++ program7.py
@@ -1,133 +1,35 @@
-# f = open("demo.txt", "r")
-# data = f.readline()
+import random
+from tkinter import *
+from tkinter import messagebox
 
-# print(data)
-# # print(type(data))
+score = 0
+run = True
 
-# line2 = f.readline()
-# print(line2)
+# main loop
+while run:
+    root = Tk()
+    root.geometry('905x700')
+    root.title('HANG MAN')
+    root.config(bg = '#E7FFFF')
+    count = 0
+    win_count = 0
 
-# line2 = f.readline()
-# print(line2)
-
-# f.close()
-
-# f = open("cake2.txt", "a")
-
-# f.write("Black Forest Cake\n")
-
-# f.close()
-
-# f = open("demo.txt", "r+")
-# f.write("check")
-# print(f.read())
-
-# f.close()
-
-# with open("demo.txt") as f:
-#     print(f.read())
-
-# with open("demo.txt", "w") as f:
-#     f.write("What you plant now, you will harvest later")
-
-# import os
-
-# os.remove("cake2.txt")
-
-#Create a new file "practice.txt" using python. Add the following data in it:
-# with open("practice.txt", "w") as f:
-#     f.write("Hi everyone\nwe are learning File I/O\nusing Java.\nI like programming in Java")
-
-#WAF  that replace all occurrences of "java" with "python" in above file.
-
-# def replace(data):
-#     new_data = data.replace("Java", "Python")
-#     with open("practice.txt", "w") as f:
-#         f.write(new_data)
-
-     
-
-# with open("practice.txt","r") as f:
-#     data = f.read()
-#     replace(data)
-
-#Search if the word "learning" exists in the file or not.
-
-# with open("practice.txt","r") as f:
-#     data = f.read()
-#     search = data.count("learning")
-#     print(search)
-#     if(search >= 1):
-#         print("the word learning exists in the file", "and it occurs", search, "time(s)")
-#     else:
-#         print("the word learning does not exist in the file")
-
-#solution from instructor
-# def check_for_word():
-#     word = "learning"
-#     with open("practice.txt", "r") as f:
-#         data = f.read()
-#         if(word in data):
-#             print("found")
-#         else:
-#             print("not found")
-
-#WAF to find in which line of the file does the word "learning" occur first.
-#print -1 if word is not found.
-# def check_for_line():
-#     with open("practice.txt", "r") as f:
-#         for el in range(1, 5):
-#             data = f.readline()
-#             learning = data.find("learning")
-#             if(learning != -1):
-#                 print("found on line: ", el)
-#             else:
-#                 print(learning)
-
-# check_for_line()
-
-#solution from the instructor
-# def check_for_line():
-#     word = "learning"
-#     data = True
-#     line_no = 1
-#     with open("practice.txt", "r") as f:
-#         while data:
-#             data = f.readline()
-#             if(word in data):
-#                 print(line_no)
-#                 return
-#             line_no += 1
-#     return -1
-
-# check_for_line()
-
-#From a file containing numbers separated by comma, print the count of even numbers.
-
-# with open("practice.txt", "r") as f:
-#     data = f.read()
-#     print(data)
-#     even = 0
-
-#     num = ""
-#     for i in range(len(data)):
-#         if(data[i] == ','):
-#             print(num)
-#             if(int(num) % 2 == 0):
-#                 even += 1
-#             num = ""
-#         else:
-#             num += data[i]
-# print("the total count of even numbers is: ", even)
-
-#solution 2 
-# count = 0
-# with open("practice.txt", "r") as f:
-#     data = f.read()
-
-#     nums = data.split(",")
-#     for val in nums:
-#         if(int(val) % 2 == 0):
-#             count += 1
-
-# print(count) 
+    # choosing word
+    index = random.randint(0,853)
+    file = open('words.txt','r')
+    l = file.readlines()
+    selected_word = l[index].strip('\n')
+    
+    # creation of word dashes variables
+    x = 250
+    for i in range(0,len(selected_word)):
+        x += 60
+        exec('d{}=Label(root,text="_",bg="#E7FFFF",font=("arial",40))'.format(i))
+        exec('d{}.place(x={},y={})'.format(i,x,450))
+        
+    #letters icon
+    al = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    for let in al:
+        exec('{}=PhotoImage(file="{}.png")'.format(let,let))
+        
+    # hangman images
